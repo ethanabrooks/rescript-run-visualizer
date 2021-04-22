29b075b5904e7d98aebe4d6c6cb38c8d70902a50
Ethan Brooks <ethanabrooks@gmail.com>
2021-04-21 20:46:23 -0400

    imported react element from chart.jsx

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const app = express()
-app.use("/", express.static(process.argv[2]))
+const express = require("express");
+const app = express();
+app.use("/", express.static(process.argv[2]));
 
-const port = 8080
+const port = 8080;
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}/`)
-})
+  console.log(`Listening on http://localhost:${port}/`);
+});
--- src/App.res
+++ src/App.res
@@ -1,2 +1,5 @@
+@module("./chart.jsx") external f: unit => string = "f"
+@module("./chart.jsx") external example: unit => React.element = "Example"
+
 @react.component
-let make = () => <div> {"hello"->React.string} </div>
+let make = (): React.element => example()
