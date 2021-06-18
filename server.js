32431f11f0a8795c50c36e339b88ae75073a729f
Ethan Brooks <ethanabrooks@gmail.com>
2021-06-18 19:15:23 +0000

    add prod and update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use("/", express.static(process.argv[2]));
 
-const port = 8080;
+const port = 8081;
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}/`);
 });
